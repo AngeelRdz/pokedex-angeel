868bb5191efeb174a551091a11a9c988c5b79c49
Jose Angel Rodriguez <joseangelrodriguez@STF-JR-303.local>
2021-08-03 13:59:34 -0500

    Caroeta build agregada

---
+++ build/precache-manifest.6c17a8c52d98d579deb55f9bac4786d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228f71f4e4e93fb8fc50ec908b88ab7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "946730e85828a175e7ac",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a7e2785705708c893a0d",
+    "url": "/static/js/2.8bc9ce40.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.8bc9ce40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946730e85828a175e7ac",
+    "url": "/static/js/main.f83b118a.chunk.js"
+  },
+  {
+    "revision": "8b53897bc9935d65a7b5",
+    "url": "/static/js/runtime-main.20a0f47d.js"
+  },
+  {
+    "revision": "6221638601ef7fa7c835eae08ef67a16",
+    "url": "/static/media/logo-pokeapi.62216386.png"
+  }
+]);
