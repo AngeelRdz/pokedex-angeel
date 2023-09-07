c1eefcc464c7f05aa63bcfc6370e895127007b30
AngeelRdz08 <angel.rodriguez@ironbit.com.mx>
2023-09-07 16:10:29 -0600

    Nueva construccion de la carpeta build.

---
+++ build/precache-manifest.01df496e3c1dcf8504899d932e85ee22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9d2539fd51dfe38a403f3682b3c724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "291a3ad2cb63e6c49925",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f55b66c4f8f2f9f376f2",
+    "url": "/static/js/2.a1eb133d.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.a1eb133d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291a3ad2cb63e6c49925",
+    "url": "/static/js/main.661f53f8.chunk.js"
+  },
+  {
+    "revision": "8b53897bc9935d65a7b5",
+    "url": "/static/js/runtime-main.20a0f47d.js"
+  },
+  {
+    "revision": "6221638601ef7fa7c835eae08ef67a16",
+    "url": "/static/media/logo-pokeapi.62216386.png"
+  }
+]);
