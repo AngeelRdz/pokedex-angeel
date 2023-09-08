135f2b82de88c0a5df3387861ca073ded9c1b079
AngeelRdz08 <angel.rodriguez@ironbit.com.mx>
2023-09-08 08:29:25 -0600

    Se realiza desde Yarn la instalacion para lograr el deploy

---
+++ build/precache-manifest.243bedbc68471f6bfdef5b1ac357dd0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8327314231d4a110452390685d3500",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d943c10ce90f73f69a9c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "abe0fe8a30a6837ba813",
+    "url": "/static/js/2.cd9635ad.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.cd9635ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d943c10ce90f73f69a9c",
+    "url": "/static/js/main.777a7f39.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.777a7f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1353d04630f6b1c7d1f",
+    "url": "/static/js/runtime-main.dd33a60a.js"
+  },
+  {
+    "revision": "6221638601ef7fa7c835eae08ef67a16",
+    "url": "/static/media/logo-pokeapi.62216386.png"
+  }
+]);
